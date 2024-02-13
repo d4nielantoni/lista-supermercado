cf22b170bc6e0a30ac9d77c6eaf5b9c62b52b07e
d4nielantoni <danielantoni7575@gmail.com>
2024-02-13 13:14:30 -0300

    primeiro commit lista de compras

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+function adicionarItem() {
+    var input = document.getElementById("itemInput");
+    var item = input.value;
+    if (item.trim() !== "") {
+        var itemList = document.getElementById("itemList");
+        var listItem = document.createElement("li");
+        listItem.textContent = item;
+        listItem.classList.add("item");
+
+        // Adicionando botão de exclusão do item
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Remover";
+        deleteButton.classList.add("deleteButton");
+        deleteButton.onclick = function () {
+            listItem.remove();
+        };
+        listItem.appendChild(deleteButton);
+
+        itemList.appendChild(listItem);
+        input.value = "";
+    } else {
+        alert("Por favor, insira um item válido.");
+    }
+}
+
+function limparLista() {
+    var itemList = document.getElementById("itemList");
+    itemList.innerHTML = "";
+}
